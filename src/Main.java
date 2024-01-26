f83d01692c2f98ad54f34cb7547009fc9908c13d
Andrey Krutskikh <ppxxd@yandex.ru>
2024-01-26 16:00:47 +0300

    <feat> customlinkedlist; removeNode; linklast; node class;

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,13 @@ public class Main {
 
         inMemoryTaskManager.createTask(new Task("test1", "test1 description"));
         inMemoryTaskManager.createTask(new Task("test2", "test2 description"));
-        inMemoryTaskManager.createTask(new Task("test3", "test3 description"));
-        inMemoryTaskManager.createTask(new Task("test4", "test4 description"));
-        inMemoryTaskManager.createTask(new Task("test5", "test5 description"));
-        inMemoryTaskManager.createTask(new Task("test6", "test6 description"));
-        inMemoryTaskManager.createTask(new Task("test7", "test7 description"));
-        inMemoryTaskManager.createTask(new Task("test8", "test8 description"));
-        inMemoryTaskManager.createTask(new Task("test9", "test9 description"));
-        inMemoryTaskManager.createTask(new Task("test10", "test10 description"));
 
-        inMemoryTaskManager.createEpicTask(new EpicTask("test11", "test11 description"));
-        inMemoryTaskManager.createEpicTask(new EpicTask("test12", "test12 description"));
+        inMemoryTaskManager.createEpicTask(new EpicTask("test3", "test3 description"));
+        inMemoryTaskManager.createEpicTask(new EpicTask("test4", "test4 description"));
 
-        inMemoryTaskManager.createSubTask(new SubTask("test13", "test13", 11));
-        inMemoryTaskManager.createSubTask(new SubTask("test14", "test14", 11));
-        inMemoryTaskManager.createSubTask(new SubTask("test15", "test15", 12));
+        inMemoryTaskManager.createSubTask(new SubTask("test5", "test5", 3));
+        inMemoryTaskManager.createSubTask(new SubTask("test6", "test6", 3));
+        inMemoryTaskManager.createSubTask(new SubTask("test7", "test7", 3));
 
         System.out.println(inMemoryTaskManager.getHistory());
 
@@ -30,27 +22,25 @@ public class Main {
         inMemoryTaskManager.getTaskById(2);
 
         System.out.println(inMemoryTaskManager.getHistory());
-        System.out.println(inMemoryTaskManager.getHistory().size());
 
-        for (int i = 3; i < 11; i++) {
-            inMemoryTaskManager.getTaskById(i);
-        }
+        inMemoryTaskManager.getEpicTaskById(3);
+        inMemoryTaskManager.getSubTaskById(5);
 
         System.out.println(inMemoryTaskManager.getHistory());
-        System.out.println(inMemoryTaskManager.getHistory().size());
 
-        inMemoryTaskManager.getEpicTaskById(11);
-
-        System.out.println(inMemoryTaskManager.getHistory());
-        System.out.println(inMemoryTaskManager.getHistory().size());
-
-        inMemoryTaskManager.getSubTaskById(13);
-        inMemoryTaskManager.getSubTaskById(15);
-        inMemoryTaskManager.getTaskById(6);
-        inMemoryTaskManager.getTaskById(5);
+        inMemoryTaskManager.getTaskById(1);
+        inMemoryTaskManager.getSubTaskById(6);
+        inMemoryTaskManager.getSubTaskById(7);
 
         System.out.println(inMemoryTaskManager.getHistory());
-        System.out.println(inMemoryTaskManager.getHistory().size());
+
+        inMemoryTaskManager.deleteEpicTaskById(3);
+
+        System.out.println(inMemoryTaskManager.getHistory());
+
+        inMemoryTaskManager.clearTasks();
+
+        System.out.println(inMemoryTaskManager.getHistory());
 
     }
 }
--- src/taskmanager/InMemoryHistoryManager.java
+++ src/taskmanager/InMemoryHistoryManager.java
@@ -3,24 +3,102 @@ package taskmanager;
 import java.util.List;
 import java.util.ArrayList;
 import tasks.Task;
+import java.util.Map;
+import java.util.HashMap;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final static int SIZE_LIST = 10;
-    private final List<Task> historyList = new ArrayList<>();
+    private List<Task> historyList = new ArrayList<>();
+    private final Map<Integer, Node> customLinkedList = new HashMap<>();
 
-    @Override
-    public void add(Task task) {
-        if (task != null) {
-            historyList.add(task);
-            if (historyList.size() > SIZE_LIST) {
-                historyList.remove(0);
+    private Node firstNode;
+    private Node lastNode;
+
+    public static class Node {
+        private Node previous;
+        private Node next;
+        private final Task value;
+
+        public Node(Node previous, Task value, Node next) {
+            this.previous = previous;
+            this.value = value;
+            this.next = next;
+        }
+
+    }
+
+    public Node linkLast(Task task) {
+        final Node previuosLast = lastNode;
+        final Node newLast = new Node (previuosLast, task, null);
+        lastNode = newLast;
+
+        if (previuosLast == null) {
+            firstNode = newLast;
+        } else {
+            previuosLast.next = newLast;
+        }
+
+        return newLast;
+    }
+
+    private void removeNode(Node node) {
+
+        if (node == lastNode) { //clearing last
+            if (lastNode.previous != null) {
+                lastNode = lastNode.previous;
+            } else {
+                lastNode = null;
+            }
+        }
+
+        if (node == firstNode) { //clearing first
+            if (firstNode.next != null) {
+                firstNode = firstNode.next;
+            } else {
+                firstNode = null;
+            }
+        }
+
+        if (node.previous != null) { //cutting between
+            node.previous.next = node.next;
+            if (node.next != null) {
+                node.next.previous = node.previous;
             }
         }
     }
 
     @Override
+    public void add(Task task) {
+        if (customLinkedList.containsKey(task.getId())) {
+            removeNode(customLinkedList.get(task.getId()));
+            customLinkedList.remove(task.getId());
+        }
+
+        Node newNode = linkLast(task);
+        customLinkedList.put(task.getId(), newNode);
+    }
+
+    @Override
+    public void remove(int id) {
+         if (customLinkedList.containsKey(id)) {
+             removeNode(customLinkedList.get(id));
+             customLinkedList.remove(id);
+         }
+    }
+
+    @Override
     public List<Task> getHistory() {
+        historyList = getTasks();
         return new ArrayList<>(historyList);
+    }
+
+    private List<Task> getTasks() {
+        ArrayList<Task> tasksArrayList = new ArrayList<>();
+        Node node = firstNode;
+        while (node != null) {
+            tasksArrayList.add(node.value);
+            node = node.next;
+        }
+        return tasksArrayList;
     }
 }
